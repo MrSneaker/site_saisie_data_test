2f810daa016f14aa8cf88a2079715da147a7fe0c
p2002495 <mateomunoz@orange.fr>
2023-03-10 21:23:37 +0100

    on commence à organiser la page.

--- client.js
+++ client.js
@@ -1,8 +1,31 @@
+
+var is_open = false;
+
 function test() {
-    let html = document.getElementById("paragraphe");
+    let html = document.querySelector("#paragraphe");
     html.innerHTML +=
         `<p>
             j'adore vivre
         </p>`;
     return { html };
+}
+
+function aff_menu() {
+    let opened = is_open;
+    let html = document.querySelector("#menu");
+    if (opened != true) {
+        html.innerHTML +=
+            `<p>
+            je retourne le menu
+        </p>`;
+        opened = true;
+        is_open = opened;
+    }
+    else {
+        html.innerHTML = `Menus`;
+        opened = false;
+        is_open = opened;
+    }
+    console.log(is_open);
+    return html;
 }
